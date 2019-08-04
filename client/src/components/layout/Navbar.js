4668aaccb6b8758d347f2eb8b00ec00ea13428bc
Alex Drozd <drozdster@gmail.com>
2019-08-04 13:22:29 -0500

    adds auth store and login form

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Navbar = ({ title, icon }) => {
   return (
@@ -11,24 +10,30 @@ const Navbar = ({ title, icon }) => {
       </h1>
       <ul>
         <li>
-          <Link to='/'>Home</Link>
+          <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to='about'>About</Link>
+          <Link to="about">About</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
         </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
 Navbar.propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string,
-}
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string
+};
 
 Navbar.defaultProps = {
-    title: 'Contact Keeper',
-    icon: 'fas fa-id-card-alt'
-}
+  title: "Contact Keeper",
+  icon: "fas fa-id-card-alt"
+};
 
 export default Navbar;

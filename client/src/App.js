e6a538ded18cfa78d5557a8197e5a0f7c0be5b8b
Alex Drozd <drozdster@gmail.com>
2019-08-04 20:30:52 -0500

    adds protected route and redirect if not authenticated

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/layout/Navbar";
 
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
+import PrivateRoute from "./components/routing/PrivateRoute";
 
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
@@ -33,7 +34,7 @@ const App = () => {
               <div className="container">
                 <Alert />
                 <Switch>
-                  <Route exact path="/" component={Home} />
+                  <PrivateRoute exact path="/" component={Home} />
                   <Route exact path="/about" component={About} />
                   <Route exact path="/register" component={Register} />
                   <Route exact path="/login" component={Login} />
